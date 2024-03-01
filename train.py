acc8ec1db3f550bcd45f5658835858ee0c4db84f
Ashish Tamhane <ashish.tamhane@gmail.com>
2024-02-29 21:28:04 -0600

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 import mlflow
-
+from mlflow.tracking import MlflowClient
+from mlflow.exceptions import MlflowException
 
 os.environ[
     "MLFLOW_TRACKING_URI"
@@ -9,7 +10,7 @@ os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
 os.environ["AWS_ACCESS_KEY_ID"] = "minioadmin"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "minioadmin"
 
-experiment_name = "demo_experiment4"
+experiment_name = "demo_experiment7"
 try:
     mlflow.create_experiment(experiment_name, artifact_location="s3://mlflow/")
 except MlflowException as e:
