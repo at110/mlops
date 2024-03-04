f49a7a636af31554fa1cd16e33139884b7d40612
Ashish Tamhane <ashish.tamhane@gmail.com>
2024-03-04 00:01:49 -0600

    Update train.py

--- train.py
+++ train.py
@@ -9,7 +9,11 @@ import mlflow
 from mlflow.tracking import MlflowClient
 from mlflow.exceptions import MlflowException
 import unittest
-import env_variables
+os.environ["MLFLOW_TRACKING_URI"] = "postgresql://user:password@localhost:5435/mlflowdb"
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+os.environ["AWS_ACCESS_KEY_ID"] = "minioadmin"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "minioadmin"
+
 
 def eval_metrics(actual: np.ndarray, pred: np.ndarray) -> tuple:
     """
