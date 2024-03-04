85d58b846a55a1ea26ff3c5e59bef313fdaa75af
Ashish Tamhane <ashish.tamhane@gmail.com>
2024-03-03 22:46:49 -0600

    Update env_variables.py

--- env_variables.py
+++ env_variables.py
@@ -1,4 +1,5 @@
 # Configure environment variables for MLflow
+import os
 os.environ["MLFLOW_TRACKING_URI"] = "postgresql://user:password@localhost:5435/mlflowdb"
 os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
 os.environ["AWS_ACCESS_KEY_ID"] = "minioadmin"
